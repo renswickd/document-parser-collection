5dadfab90b7d5c207cde1ce8ca85f7a947702dbc
renswickd <renswick.delver@gmail.com>
2025-06-05 23:23:03 +1200

    Blob storage connection established.

--- parser/azure-doc-intelligence.py
+++ parser/azure-doc-intelligence.py
@@ -3,6 +3,23 @@ from dotenv import load_dotenv
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+from azure.storage.blob import BlobServiceClient
+
+def get_url_from_blob():
+    load_dotenv()
+
+    # Retrieve the connection string from environment variables
+    connection_string = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
+    container_name = os.environ["AZURE_STORAGE_CONTAINER_NAME"]
+    blob_name = os.environ["AZURE_STORAGE_BLOB_NAME"]
+    
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob_name)
+    url = blob_client.url
+
+    return url
+
 
 def analyze_layout_from_url():
     
